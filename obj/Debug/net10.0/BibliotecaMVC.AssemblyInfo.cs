57247c710a7bf32af3db25acbd352de1a571ec1e
Dereck Méndez <mayorickhenry@gmail.com>
2026-09-03 20:23:54 -0600

    Commit de prueba

--- obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063290c3b3125730e12b6bbcf6f05f0046d558d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a88a302ded1993e086b3fe9dfec264d707e89f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
